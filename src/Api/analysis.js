4dfc6edc79f3fff72f7f85c78b162571ce430372
Dhanush.shetty <dhanush.shetty@cimpress.com>
2025-07-10 18:07:35 +0530

    update api

--- src/Api/analysis.js
+++ src/Api/analysis.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 export const analyzeRepo = async (payload) => {
-  const res = await axios.post('http://localhost:3001/analysis/repo', payload);
+  const res = await axios.post('https://codebroom-api.onrender.com/analysis/repo', payload);
   return res.data;
 };
 
 export const analyzeFile = async (payload) => {
-    const res = await axios.post('http://localhost:3001/analysis/file', payload);
+    const res = await axios.post('https://codebroom-api.onrender.com/analysis/file', payload);
     return res.data;
 };
